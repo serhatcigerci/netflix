49c3e048347607d80648e6ef25673bde51a488b6
serhatcigerci <yserhatcigerci@gmail.com>
2022-07-12 14:44:08 +0300

    logo finished

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Notifications, Search } from "@material-ui/icons"
 import "./navbar.css"
 
 const Navbar = () => {
@@ -5,14 +6,18 @@ const Navbar = () => {
     <div className="navbar">
       <div className="container">
         <div className="left">
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png" alt=""/>
+        <img src='/assets/images/logo.png' alt="logo"/>
           <span>Homepage</span>
           <span>Series</span>
           <span>Movies</span>
           <span>New and Popular</span>
           <span>My List</span>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <Search/>
+          <span>KID</span>
+          <Notifications/>
+        </div>
       </div>
     </div>
   )
