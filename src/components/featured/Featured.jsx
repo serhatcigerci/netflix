35fae4d7f145d20b9c5488f72e13f9d9750ad062
serhatcigerci <yserhatcigerci@gmail.com>
2022-07-15 12:26:44 +0300

    Featured part

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,12 @@
+import './featured.css'
+
+const Featured = () => {
+  return (
+    <div className="featured">
+      <img width="100%"src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
+
+    </div>
+  )
+}
+
+export default Featured
