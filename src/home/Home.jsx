240301a387b3a0725f9fe3ac6a7ec2c42fb9ebc2
serhatcigerci <yserhatcigerci@gmail.com>
2022-07-19 12:01:40 +0300

    list added

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,4 +1,5 @@
 import Featured from "../components/featured/Featured"
+import List from "../components/list/List"
 import Navbar from "../components/navbar/Navbar"
 import "./home.css"
 
@@ -6,7 +7,8 @@ const Home = () => {
   return (
     <div className="home">
       <Navbar />
-      <Featured type="movie"/>
+      <Featured/>
+      <List />
     </div>
   )
 }
