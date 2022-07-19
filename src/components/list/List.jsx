99a1b769b844a43352eb7730b1331767bf4e9781
serhatcigerci <yserhatcigerci@gmail.com>
2022-07-19 14:04:32 +0300

    List item

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,9 +1,17 @@
+import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from '@material-ui/icons'
 import React from 'react'
 
 const List = () => {
   return (
     <div className="list">
-      list
+      <span className="listTitle">Continue to watch</span>
+      <div className="wrapper">
+        <ArrowBackIosOutlined/>
+        <div className="container">
+          
+        </div>
+        <ArrowForwardIosOutlined/>
+      </div>
     </div>
   )
 }
