3252c2eb803f14cf75bff20fe2a9912f3a312919
serhatcigerci <yserhatcigerci@gmail.com>
2022-09-01 14:29:00 +0300

    Left sliders functions added with useRef

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,14 +1,23 @@
 import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from '@material-ui/icons'
 import './list.css'
 import ListItem from '../listItem/ListItem'
+import { useRef } from 'react'
 
 const List = () => {
+  
+  const listRef = useRef()
+
+  const handleClick = (direction) => {
+    if(direction === "left") {
+      listRef.current.style.transform = `translateX(230px)`
+    }
+  }
   return (
     <div className="list">
       <span className="listTitle">Continue to watch</span>
       <div className="wrapper">
-        <ArrowBackIosOutlined className="sliderArrow left"/>
-        <div className="container">
+        <ArrowBackIosOutlined className="sliderArrow left" onClick={() => handleClick("left")}/>
+        <div className="container" ref={listRef}>
           <ListItem/>
           <ListItem/>
           <ListItem/>
@@ -22,7 +31,7 @@ const List = () => {
           <ListItem/>
           <ListItem/>
         </div>
-        <ArrowForwardIosOutlined className="sliderArrow right"/>
+        <ArrowForwardIosOutlined className="sliderArrow right"onClick={() => handleClick("right")}/>
       </div>
     </div>
   )
