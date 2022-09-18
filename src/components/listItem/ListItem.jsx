3a33001f9b8db1f75b624020b37710c981e4f698
serhatcigerci <yserhatcigerci@gmail.com>
2022-09-18 17:06:44 +0300

    listItem hover effect and buttons

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,8 +1,18 @@
+import { Add, PlayArrow, ThumbDownAltOutlined, ThumbUpAltOutlined } from '@material-ui/icons'
 import './listItem.css'
 
 const ListItem = () => {
   return (
-    <div className="listItem"><img src='/assets/images/entrapped.jpg' alt="entrapped"/></div>
+    <div className="listItem"><img src='/assets/images/entrapped.jpg' alt="entrapped"/>
+      <div className="itemInfo">
+        <div className="icons">
+          <PlayArrow/>
+          <Add/>
+          <ThumbUpAltOutlined/>
+          <ThumbDownAltOutlined/>
+        </div>
+      </div>
+   </div>
   )
 }
 
